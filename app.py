23afb5be4536232fc4f82a22bf9208543643505b
João Ortiz <joao.ortiz1998@gmail.com>
2024-04-12 00:25:16 -0300

    Api finalizada

--- app.py
+++ app.py
@@ -4,16 +4,75 @@ import firebase_admin
 from firebase_admin import credentials, auth, firestore, exceptions
 import psycopg2
 from psycopg2.extras import RealDictCursor
+from sqlalchemy import create_engine, Column, Integer, String, ARRAY
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+import os
+
+engine = create_engine('postgresql://postgres:phd@localhost:5432/api-netflix')
+
+Base = declarative_base()
+
+class Usuario(Base):
+    __tablename__ = 'sys_usuario'
+
+    usu_co_usuario = Column(Integer, primary_key=True)
+    usu_no_nome = Column(String(60), nullable=False)
+    usu_no_email = Column(String(60), nullable=False, unique=True)
+    usu_in_status = Column(String(1), nullable=False)
+
+class Volume(Base):
+    __tablename__ = 'sys_volumes'
+
+    vol_co_volume = Column(Integer, primary_key=True)
+    vol_no_volume = Column(String(45), nullable=False)
+    vol_in_status = Column(String(1))
+    vol_tp_volume = Column(String(1))
+    vol_tx_small_descricao = Column(String(300))
+    vol_tx_sinopse = Column(String(5000))
+    vol_tx_elenco = Column(String(1000))
+    vol_tx_diretor = Column(String(40))
+    vol_av_avaliacao = Column(String(1))
+    vol_tp_genero = Column(String(25))
+    vol_nu_classificacao = Column(String(2))
+
+class VolumeReproducao(Base):
+    __tablename__ = 'sys_volumes_reproducao'
+
+    volr_co_reproducao = Column(Integer, primary_key=True)
+    volr_no_titulo = Column(String(45), nullable=False)
+    usu_co_usuario = Column(Integer, nullable=False)
+    volr_co_volume = Column(Integer, nullable=False)
+    volr_tp_volume = Column(String(1))
+    volr_ep_temp = Column(String(5))
+    usuario = relationship("Usuario")
+    volume = relationship("Volume")
+
+class ListaReproducao(Base):
+    __tablename__ = 'sys_lista_reproducao'
+
+    lrep_co_lista = Column(Integer, primary_key=True)
+    usu_co_usuario = Column(Integer, nullable=False)
+    vol_co_volume = Column(ARRAY(Integer))
+    lrep_no_lista = Column(String(100))
+    lrep_in_status = Column(String(1))
+
+Base.metadata.create_all(engine)
 
 app = Flask(__name__)
 
 cred = credentials.Certificate("./api-netflix-py-firebase.json")  
 firebase_admin.initialize_app(cred)
-FIREBASE_WEB_API_KEY = 'AIzaSyAKIdt6Ua9tOxuKth3791lgf_zd_Ef8exs'
+FIREBASE_WEB_API_KEY = os.getenv("FIREBASE_WEB_API_KEY")
 
 def connect_to_db():
-    return psycopg2.connect(host="localhost", dbname="netflix", user="postgres", password="1997")
-
+    return psycopg2.connect(
+        host=os.getenv("DB_HOST"),
+        dbname=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        port=os.getenv("DB_PORT")
+    )
 
 @app.route('/')
 def index():
@@ -150,9 +209,6 @@ def historico_visualizacao(usu_co_usuario):
         auth_header = request.headers.get('Authorization')
         user_id = verify_firebase_token(auth_header)
 
-        if str(usu_co_usuario) != str(user_id):
-            return jsonify({'message': 'Não autorizado.'}), 403
-
         conn = connect_to_db()
         cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
         cur.execute("""
@@ -177,6 +233,130 @@ def historico_visualizacao(usu_co_usuario):
         return jsonify({'message': 'Token de autenticação inválido.'}), 401
     except Exception as e:
         return jsonify({'error': str(e)}), 500
+    
+@app.route('/reproducao', methods=['POST'])
+def registrar_reproducao():
+    try:
+        data = request.get_json()
+
+        if not data or 'user_id' not in data or 'titulo_id' not in data:
+            return jsonify({'message': 'ID do usuário e ID do título são obrigatórios!'}), 400
+
+        auth_header = request.headers.get('Authorization')
+        user_id = verify_firebase_token(auth_header)
+        
+
+        conn = connect_to_db()
+        cur = conn.cursor()
+        cur.execute("SELECT COUNT(*) FROM sys_usuario WHERE usu_co_usuario = %s", (data['user_id'],))
+        user_count = cur.fetchone()[0]
+        cur.execute("SELECT COUNT(*) FROM sys_volumes WHERE vol_co_volume = %s", (data['titulo_id'],))
+        titulo_count = cur.fetchone()[0]
+        cur.close()
+        conn.close()
+
+        if user_count == 0 or titulo_count == 0:
+            return jsonify({'message': 'Usuário ou título não encontrado.'}), 404
+
+        conn = connect_to_db()
+        cur = conn.cursor()
+        cur.execute("""
+            INSERT INTO sys_volumes_reproducao (volr_no_titulo, usu_co_usuario, volr_co_volume, volr_tp_volume)
+            VALUES (
+                (SELECT vol_no_volume FROM sys_volumes WHERE vol_co_volume = %s),
+                %s, %s, (SELECT vol_tp_volume FROM sys_volumes WHERE vol_co_volume = %s)
+            )
+        """, (data['titulo_id'], data['user_id'], data['titulo_id'], data['titulo_id']))
+        conn.commit()
+        cur.close()
+        conn.close()
+
+        return jsonify({'message': 'Reprodução registrada com sucesso!'}), 201
+    except ValueError as ve:
+        return jsonify({'message': str(ve)}), 401
+    except exceptions.AuthError:
+        return jsonify({'message': 'Token de autenticação inválido.'}), 401
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+@app.route('/busca', methods=['POST'])
+def buscar_titulos():
+    try:
+        auth_header = request.headers.get('Authorization')
+        user_id = verify_firebase_token(auth_header)
+
+        data = request.get_json()
+
+        query = "SELECT vol_no_volume, vol_tx_small_descricao FROM sys_volumes WHERE vol_in_status = 'A'"
+
+        if data:
+            query_conditions = []
+
+            if data.get('genero'):
+                query_conditions.append("vol_tp_genero LIKE %s")
+            if data.get('ano_lancamento'):
+                query_conditions.append("EXTRACT(YEAR FROM vol_dt_lancamento) = %s")
+            if data.get('classificacao'):
+                query_conditions.append("vol_nu_classificacao = %s")
+            
+            if query_conditions:
+                query += " AND " + " AND ".join(query_conditions)
+
+        conn = connect_to_db()
+        cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+
+        params = []
+        if data:
+            if data.get('genero'):
+                params.append("%" + data['genero'] + "%")
+            if data.get('ano_lancamento'):
+                params.append(data['ano_lancamento'])
+            if data.get('classificacao'):
+                params.append(data['classificacao'])
+
+        cur.execute(query, params)
+        titulos = cur.fetchall()
+        cur.close()
+        conn.close()
+
+        result = [{'titulo': titulo['vol_no_volume'], 'descricao': titulo['vol_tx_small_descricao']} for titulo in titulos]
+        return jsonify(result), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+@app.route('/lista_reproducao', methods=['POST'])
+def criar_lista_reproducao():
+    try:
+        auth_header = request.headers.get('Authorization')
+        verified_user_id = verify_firebase_token(auth_header)
+
+        data = request.get_json()
+
+        if not data or 'user_id' not in data  or 'nome_lista' not in data or 'codigos_volumes' not in data:
+            return jsonify({'message': 'Token, ID do usuário, nome da lista e códigos dos volumes são obrigatórios!'}), 400
+
+        
+        
+        conn = connect_to_db()
+        cur = conn.cursor()
+        cur.execute("""
+            INSERT INTO sys_lista_reproducao (usu_co_usuario, vol_co_volume, lrep_no_lista, lrep_in_status)
+            VALUES (%s, %s, %s, 'A')
+        """, (data['user_id'], data['codigos_volumes'], data['nome_lista']))
+        conn.commit()
+        cur.close()
+        conn.close()
+
+        return jsonify({'message': 'Lista de reprodução criada com sucesso!'}), 201
+    except ValueError as ve:
+        return jsonify({'message': str(ve)}), 401
+    except exceptions.AuthError:
+        return jsonify({'message': 'Token de autenticação inválido.'}), 401
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
 
 
 if __name__ == '__main__':
